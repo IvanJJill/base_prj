c098e67301b1bb6da762f798291f8eb6f4e5fd2e
ivan <ivanjjill@gmail.com>
2015-03-15 22:16:56 +0000

    bicycle model added

--- bicycles/models.py
+++ bicycles/models.py
@@ -8,6 +8,6 @@ class Bicycle(models.Model):
     type_height = models.IntegerField(default=1)  # height category - depend on frame/gender will be sets of sizes
     description = models.TextField()  # text description of the bike. in html
     available = models.BinaryField()  # available or in use right now
-    occupied_order = models.ForeignKey()  # order id that the bike is occupied now
+    # occupied_order = models.ForeignKey()  # order id that the bike is occupied now
     image = models.ImageField()  # bike image for web
     status = models.IntegerField(default=0)  # technical status - new, worn, to maintenance etc.
