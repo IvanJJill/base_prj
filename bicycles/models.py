abd2455f2e4c259df86d8676d432deb79b88dedb
ivan <ivanjjill@gmail.com>
2015-03-17 22:20:17 +0000

    bugfix. Testing admin panel

--- bicycles/models.py
+++ bicycles/models.py
@@ -7,13 +7,12 @@ class Bicycle(models.Model):
     type_model = models.CharField('manufacturer model', max_length=20)  # manufacturer model
     type_frame = models.IntegerField('frame type', default=1)  # frame type = road/mountain/fixie/etc
     type_gender = models.IntegerField('gender type', default=1)  # male/fem/uni/child
-    type_height = models.IntegerField('height category', default=1)  # height category - depend on frame/gender will be sets of sizes
-    description = models.TextField('text description', )  # text description of the bike. in html
-    available = models.BinaryField('availability', )  # available or in use right now
-    # occupied_order = models.ForeignKey(Order)  # order id that the bike is occupied now
-    image = models.CharField('bike image for web', max_length=256)  # models.ImageField()  # bike image for web
+    type_height = models.IntegerField('height category', default=1)  # depend on frame/gender will be sets of sizes
+    description = models.TextField('text description')  # text description of the bike. in html
+    available = models.BinaryField('availability')  # available or in use right now
+    images = models.CharField('bike image for web', max_length=256)  # models.ImageField()  # bike image for web
     status = models.IntegerField('maintenance status', default=0)  # technical status - new, worn, to maintenance etc.
-    value = models.FloatField('Value in money', )  # bike value in f.ex. EUR to calculate the price
+    value = models.FloatField('value in money')  # bike value in f.ex. EUR to calculate the price
 
     """to return a name of itself when called from anywhere"""
     def __str__(self):
