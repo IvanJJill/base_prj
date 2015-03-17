9156d6173b6ef480dbbb93ad7edbc6cb1515a221
ivan <ivanjjill@gmail.com>
2015-03-17 22:27:22 +0000

    bugfix. Testing admin panel

--- bicycles/models.py
+++ bicycles/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 
 class Bicycle(models.Model):
-    title = models.CharField(u"bicycle title", max_length=60)  # bicycle title
-    type_make = models.CharField('bicycle manufacturer', max_length=20)  # bicycle manufacturer
-    type_model = models.CharField('manufacturer model', max_length=20)  # manufacturer model
+    title = models.CharField(u"bicycle title", max_length=60, default='')  # bicycle title
+    type_make = models.CharField('bicycle manufacturer', max_length=20, default='')  # bicycle manufacturer
+    type_model = models.CharField('manufacturer model', max_length=20, default='')  # manufacturer model
     type_frame = models.IntegerField('frame type', default=1)  # frame type = road/mountain/fixie/etc
     type_gender = models.IntegerField('gender type', default=1)  # male/fem/uni/child
     type_height = models.IntegerField('height category', default=1)  # depend on frame/gender will be sets of sizes
-    description = models.TextField('text description')  # text description of the bike. in html
-    available = models.BinaryField('availability')  # available or in use right now
-    images = models.CharField('bike image for web', max_length=256)  # models.ImageField()  # bike image for web
+    description = models.TextField('text description', default='default desciption')  # text description of the bike
+    available = models.BinaryField('availability', default=True)  # available or in use right now
+    images = models.CharField('bike image for web', max_length=256, default='')  # bike image for web
     status = models.IntegerField('maintenance status', default=0)  # technical status - new, worn, to maintenance etc.
-    value = models.FloatField('value in money')  # bike value in f.ex. EUR to calculate the price
+    value = models.FloatField('value in money', default=0.0)  # bike value in f.ex. EUR to calculate the price
 
     """to return a name of itself when called from anywhere"""
     def __unicode__(self):
