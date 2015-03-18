d5b42eb506ffe7a4ec87a9fe996e16364f507950
ivan <ivanjjill@gmail.com>
2015-03-18 23:26:37 +0000

    adding models to admin panel

--- bicycles/models.py
+++ bicycles/models.py
@@ -2,16 +2,38 @@ from django.db import models
 
 
 class Bicycle(models.Model):
+
+    MAINTENANCE= (
+        (1, 'Ready'),
+        (2, 'Broken light'),
+        (3, 'Broken heavily'),
+        (4, 'Stolen'),
+        (5, 'In repair'),
+        (6, 'Unknown'),
+    )
+
+    SIZE = (
+        (1, 'Ladies L'),
+        (2, 'Ladies M'),
+        (3, 'Ladies S'),
+        (4, 'Gents L'),
+        (5, 'Gents M'),
+        (6, 'Gents S'),
+        (7, 'Child L'),
+        (8, 'Child M'),
+        (9, 'Child S'),
+        (99, 'Unknown')
+    )
     title = models.CharField(u"bicycle title", max_length=60, default='')  # bicycle title
-    type_make = models.CharField('bicycle manufacturer', max_length=20, default='')  # bicycle manufacturer
-    type_model = models.CharField('manufacturer model', max_length=20, default='')  # manufacturer model
-    type_frame = models.IntegerField('frame type', default=1)  # frame type = road/mountain/fixie/etc
+    type_make = models.CharField('bicycle manufacturer', max_length=20, default='', blank=True)  # bicycle manufacturer
+    type_model = models.CharField('manufacturer model', max_length=20, default='', blank=True)  # manufacturer model
+    type_frame = models.IntegerField('frame type', default=1, blank=True)  # frame type = road/mountain/fixie/etc
     type_gender = models.IntegerField('gender type', default=1)  # male/fem/uni/child
-    type_height = models.IntegerField('height category', default=1)  # depend on frame/gender will be sets of sizes
+    type_height = models.IntegerField('height category', default=1, max_length=2, choices=SIZE)  # depend on frame/gender will be sets of sizes
     description = models.TextField('text description', default='default description')  # text description of the bike
     available = models.NullBooleanField(default=False)  # available or in use right now
     images = models.CharField('bike image for web', max_length=256, default='')  # bike image for web
-    status = models.IntegerField('maintenance status', default=0)  # technical status - new, worn, to maintenance etc.
+    status = models.IntegerField('maintenance status', default=0, max_length=2, choices=MAINTENANCE)  # technical status - new, worn, to maintenance etc.
     value = models.FloatField('value in money', default=0.0)  # bike value in f.ex. EUR to calculate the price
 
     """to return a name of itself when called from anywhere"""
@@ -26,12 +48,15 @@ class Client(models.Model):
         ('Miss', 'Ms'),
     )
 
-    title = models.CharField('Client title', max_length=1, choices=TITLES)
+    title = models.CharField('Client title', max_length=10, choices=TITLES)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.CharField(max_length=60)
     phone = models.CharField(max_length=12)
     location = models.CharField(max_length=12, blank=True)
+
+    def __str__(self):
+        return self.title + self.first_name + self.last_name
 
 
 class Order(models.Model):
@@ -45,4 +70,7 @@ class Order(models.Model):
     paid = models.NullBooleanField(default=False)
     invoiced = models.NullBooleanField(default=False)
     price = models.FloatField(default=0.0)  # on order create price will be calculated depending on order date/days/value
+
+    def __str__(self):
+        return self.id + self.client
 
