a1b27cbcb60880e06e82a6fc99cce0f506dd9d11
ivan <ivanjjill@gmail.com>
2015-03-18 23:06:13 +0000

    bugfix. Testing admin panel

--- base_prj/urls.py
+++ base_prj/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import bicycles
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'base_prj.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
 
     url(r'^$', 'base_prj.views.home', name='home'),
     url(r'^bikes/', include(bicycles.urls), name='bikes'),
