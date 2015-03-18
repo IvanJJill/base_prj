3afc9625f6f6dc99176c8ce75fcb3adf7cc71ce4
ivan <ivanjjill@gmail.com>
2015-03-18 23:13:56 +0000

    adding models to admin panel

--- bicycles/admin.py
+++ bicycles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from bicycles.models import Bicycle
+from bicycles.models import Client
+from bicycles.models import Order
 
+admin.site.register(Bicycle)
+admin.site.register(Client)
+admin.site.register(Order)
 # Register your models here.
