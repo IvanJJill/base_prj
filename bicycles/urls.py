665f3ee49f1c37d701ace431b246a8a769f7e7be
ivan <ivanjjill@gmail.com>
2015-03-26 21:57:50 +0000

    making test view

--- bicycles/urls.py
+++ bicycles/urls.py
@@ -3,6 +3,6 @@ from bicycles import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
-                       url(r'^bikes(/?P<bike_id>\d+)/$', views.bike_details, name='bikes'),
-                       url(r'^orders(/?P<order_id>\d+)/$', views.order_details, name='orders'),
+                       url(r'^(?P<bike_id>\d+)/$', views.bike_details, name='bikes'),
+                       url(r'^orders(?P<order_id>\d+)/$', views.order_details, name='orders'),
                        )
