a627927eb71da020306dada4353dde97aa2a51dd
ivan <ivanjjill@gmail.com>
2015-04-13 07:45:11 +0100

    trying views

--- bicycles/views.py
+++ bicycles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template import RequestContext, loader
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404
 from bicycles.models import Bicycle, Order, Client
 
 
@@ -8,15 +9,15 @@ def index(request):
     bicycles_list = Bicycle.objects.order_by('-type_make')[:5]
     template = loader.get_template('bicycles/index.html')
     context = RequestContext(request, {
-        'bicycles_list':bicycles_list,
+        'bicycles_list': bicycles_list,
 
     })
     return HttpResponse(template.render(context))
 
 
 def bike_details(request, bike_id):
-    response = "Read bike's %s details"
-    return HttpResponse(response % bike_id)
+    bike = get_object_or_404(Bicycle, pk=bike_id)
+    return render(request, 'bicycles/bike_details.html', {'bike': bike})
 
 
 def order_details(request, order_id):
