e31364204e74f9e045972810ea307a2095d4ab86
ivan <ivanjjill@gmail.com>
2015-04-20 23:07:58 +0100

    trying views

--- base_prj/settings.py
+++ base_prj/settings.py
@@ -83,4 +83,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "base_prj"),)  # added line to enable dynamic routing for static files
+TEMPLATE_DIRS = [os.path.join(os.path.join(BASE_DIR, "base_prj"), "templates")]
--- base_prj/views.py
+++ base_prj/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the bikes index.")
+    return render(request, 'base.html')
